1a53b3bd6a154c66e31bda8e08f1044d7298cb84
Nikita Kisly <sour945@gmail.com>
2023-02-07 16:06:29 +0300

    feat: add switch cases for socket

--- controllers/websocket-controller.js
+++ controllers/websocket-controller.js
@@ -1,6 +1,8 @@
 export class WsController {
   constructor(wsInstance) {
     this.wsInstance = wsInstance;
+    this.games = {};
+    this.info = wsInstance.getWss();
   }
 
   webSocketHandler(ws, req) {
@@ -12,22 +14,51 @@ export class WsController {
           this.connectHandler(ws, msg);
           break;
 
-        default:
+        case "ready":
+          this.readyHandler(ws, msg);
+          break;
+
+        case "shot":
+          this.shotHandler(ws, msg);
+          break;
+
+        case "gameOver":
+          this.gameOverHandler(ws, msg);
           break;
       }
     });
   }
 
   connectHandler(ws, msg) {
-    ws.id = msg.id;
+    ws.userId = msg.userId;
+    ws.gameId = msg.gameId;
+
+    const game = this.games[msg.gameId];
+
+    if (!game) {
+      game = [ws];
+    }
+
+    if (game.length < 2) {
+      game.push(ws);
+      msg.startGame = true;
+    }
+
+    if (game.length === 2) {
+      const replaceUser = game.findIndex((user) => user.userId === msg.userId);
+      if (replaceUser >= 0) {
+        game[replaceUser] = [ws];
+      }
+    }
+
     this.connectBroadcast(ws, msg);
   }
 
   connectBroadcast(ws, msg) {
-    this.wsInstance.getWss().clients.forEach((client) => {
-        if (client.id === msg.id) {
-            client.send(`Подключение по ${msg.id}`);
-        }
+    this.info.clients.forEach((client) => {
+      if (client.gameId === msg.gameId) {
+        client.send(JSON.stringify(msg));
+      }
     });
   }
 }
